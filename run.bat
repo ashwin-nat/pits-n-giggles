b37cd7bb0f9b18a0ad79435b9b65ffd9cec2cbec
Ashwin Natarajan <ashwin.nat@gmail.com>
2024-03-25 19:34:02 +0530

    made --post-race-data-autosave on by default in helper script changed from python3 to python in helper script

--- run.bat
+++ run.bat
@@ -2,14 +2,14 @@
 setlocal
 
 REM Check if Python is installed
-where python3 > nul 2>nul
+where python > nul 2>nul
 if %errorlevel% neq 0 (
     echo Python not found. Please install Python before running this application.
     exit /b 1
 )
 
 REM Run the Python script
-python3 app.py --packet-capture-mode disabled --telemetry-port 20777 --server-port 5000
+python app.py --packet-capture-mode disabled --telemetry-port 20777 --server-port 5000 --post-race-data-autosave
 
 REM Pause to keep the command prompt window open
 pause
--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Check if Python 3 is installed
-if command -v python3 &> /dev/null; then
+if command -v python &> /dev/null; then
     echo "Python 3 is installed."
 else
     echo "Python 3 is not installed. Please install Python 3 before proceeding."
     exit 1
 fi
 
-python3 app.py --packet-capture-mode disabled --telemetry-port 20777 --server-port 5000
+python app.py --packet-capture-mode disabled --telemetry-port 20777 --server-port 5000 --post-race-data-autosave
