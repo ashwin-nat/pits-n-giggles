d9010cd66660a54ee93566e2e117116e21e6a0ce
Ashwin Natarajan <ashwin.nat@gmail.com>
2024-06-05 12:53:55 +0530

    input validation bug fix

--- src/telemetry_server.py
+++ src/telemetry_server.py
@@ -265,7 +265,7 @@ class TelemetryWebServer:
         """
 
         # Check if only one parameter is provided
-        if not param:
+        if param is None:
             return {
                 'error': 'Invalid parameters',
                 'message': f'Provide "{param_name}" parameter'
