79b7b31a7ff0a4c256ff781531ab229954be67a6
Ashwin Natarajan <ashwin.nat@gmail.com>
2025-02-08 13:39:03 +0530

    Added speed trap to stream overlay
    Added speed trap in kmph to stream overlay
        in f1 23, this div will be hidden
    Made lap history table in stream overlay have high opacity for readability
    Made the purple text a bit brighter for readabilty

--- src/static/js/penaltiesStatsOverlay.js
+++ src/static/js/penaltiesStatsOverlay.js
@@ -1,43 +1,50 @@
 class PenaltiesStatsWidget {
-    constructor() {
-      // Elements for displaying the penalties stats
-      this.pitLaneSpeedLimitSpan = document.getElementById('pit-speed-limit');
-      this.trackTempSpan = document.getElementById('track-temp');
-      this.airTempSpan = document.getElementById('air-temp');
-      this.trackLimitsWarningsSpan = document.getElementById('track-limits-warnings');
-    }
-
-    // Method to update the UI with the penalties stats
-    update(penaltiesStats, gameYear) {
-      // Update the individual penalty stats
-      this.updatePenaltiesStats(penaltiesStats, gameYear);
-    }
-
-    // Method to update the penalties stats in the UI
-    updatePenaltiesStats(pensStats, gameYear) {
-      const airTemp = pensStats['air-temperature'];
-      const trackTemp = pensStats['track-temperature'];
-      const trackLimits = pensStats['corner-cutting-warnings'];
-
-      // Set air and track temperature values or default to "---" if not available
-      this.airTempSpan.textContent = (airTemp !== null && airTemp !== undefined) ? `${airTemp} °C` : "---";
-      this.trackTempSpan.textContent = (trackTemp !== null && trackTemp !== undefined) ? `${trackTemp} °C` : "---";
-      this.trackLimitsWarningsSpan.textContent = (trackLimits !== null && trackLimits !== undefined) ? trackLimits : "---";
-
-      // Update Pit Lane Speed Limit
-      const pitLaneSpeedLimit = pensStats['pit-speed-limit'];
-      this.pitLaneSpeedLimitSpan.textContent = (pitLaneSpeedLimit !== null && pitLaneSpeedLimit !== undefined) ? pitLaneSpeedLimit : "--";
-
-      // Ensure pit speed limit element is visible if there's a value
-      this.pitLaneSpeedLimitSpan.style.display = (pitLaneSpeedLimit !== null && pitLaneSpeedLimit !== undefined) ? "inline-flex" : "none";
-    }
+  constructor() {
+    // Elements for displaying the penalties stats
+    this.pitLaneSpeedLimitSpan = document.getElementById('pit-speed-limit');
+    this.trackTempSpan = document.getElementById('track-temp');
+    this.airTempSpan = document.getElementById('air-temp');
+    this.trackLimitsWarningsSpan = document.getElementById('track-limits-warnings');
+    this.speedTrapSpan = document.getElementById('speed-trap-span');
+    this.speedTrapDiv = document.getElementById('speed-trap-div');
   }
 
-  // Usage:
-  const penaltiesWidget = new PenaltiesStatsWidget('penaltiesStatsContainer');
-  penaltiesWidget.update({
-    'air-temperature': 25,
-    'track-temperature': 30,
-    'corner-cutting-warnings': 2,
-    'pit-speed-limit': 80
-  });
+  // Method to update the UI with the penalties stats
+  update(penaltiesStats, gameYear) {
+    // Update the individual penalty stats
+    this.updatePenaltiesStats(penaltiesStats, gameYear);
+  }
+
+  // Method to update the penalties stats in the UI
+  updatePenaltiesStats(pensStats, gameYear) {
+    const airTemp = pensStats['air-temperature'];
+    const trackTemp = pensStats['track-temperature'];
+    const trackLimits = pensStats['corner-cutting-warnings'];
+    const speedTrap = pensStats['speed-trap-record'];
+
+    // Set air and track temperature values or default to "---" if not available
+    this.airTempSpan.textContent = (airTemp !== null && airTemp !== undefined) ? `${airTemp} °C` : "---";
+    this.trackTempSpan.textContent = (trackTemp !== null && trackTemp !== undefined) ? `${trackTemp} °C` : "---";
+    this.trackLimitsWarningsSpan.textContent = (trackLimits !== null && trackLimits !== undefined) ? trackLimits : "---";
+
+    // Update Pit Lane Speed Limit
+    const pitLaneSpeedLimit = pensStats['pit-speed-limit'];
+    this.pitLaneSpeedLimitSpan.textContent = (pitLaneSpeedLimit !== null && pitLaneSpeedLimit !== undefined) ? pitLaneSpeedLimit : "--";
+
+    // Ensure pit speed limit element is visible if there's a value
+    this.pitLaneSpeedLimitSpan.style.display = (pitLaneSpeedLimit !== null && pitLaneSpeedLimit !== undefined) ? "inline-flex" : "none";
+
+    // speed trap support from F1 24 onwards
+    if (gameYear >= 24) {
+      this.speedTrapDiv.style.display = "";
+      if (speedTrap !== null && speedTrap !== undefined && speedTrap !== 0.0) {
+        this.speedTrapSpan.textContent = formatFloatWithTwoDecimals(speedTrap);
+      } else {
+        this.speedTrapSpan.textContent = "---";
+      }
+    } else {
+      this.speedTrapDiv.style.display = "none";
+    }
+  }
+}
+
