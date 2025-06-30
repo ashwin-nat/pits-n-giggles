1fa537818c3d2a978d99f04096ef3f70b270ce8e
Ashwin Natarajan <ashwin.nat@gmail.com>
2025-06-30 21:56:07 +0530

    added speed trap records to post race viewer

--- apps/save_viewer/telemetry_post_race_data_viewer.py
+++ apps/save_viewer/telemetry_post_race_data_viewer.py
@@ -650,6 +650,7 @@ def handleRaceInfoRequest() -> Tuple[Dict[str, Any], HTTPStatus]:
             "overtakes" : g_json_data.get("overtakes", None),
             "custom-markers" : g_json_data.get("custom-markers", []),
             "position-history" : g_json_data.get("position-history", []),
+            "speed-trap-records" : g_json_data.get("speed-trap-records", None),
         }
 
         if new_style := g_json_data.get("tyre-stint-history-v2"):
