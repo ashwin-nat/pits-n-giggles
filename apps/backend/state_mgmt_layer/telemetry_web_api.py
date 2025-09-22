7dec9baadc628089e68313fa4e23a1935948ed2e
Ashwin Natarajan <ashwin.nat@gmail.com>
2025-09-22 15:58:20 +0530

    send lap num from backend

--- apps/backend/state_mgmt_layer/telemetry_web_api.py
+++ apps/backend/state_mgmt_layer/telemetry_web_api.py
@@ -1022,6 +1022,7 @@ class DriversListRsp:
             "sector" : str(driver_data.m_lap_info.m_curr_sector),
             "driver-status" : str(driver_data.m_lap_info.m_curr_status),
             "sector-status" : driver_data.getCurrLapSectorStatus(self.m_fastest_s1_ms, self.m_fastest_s2_ms),
+            "lap-num" : driver_data.m_lap_info.m_current_lap,
         }
 
     def _getWarningsPenaltiesJSON(self, driver_data: DataPerDriver) -> Dict[str, Any]:
