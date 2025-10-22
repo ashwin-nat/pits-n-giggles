09f9d170eb89a78bbdb99092acc7fb0cf09407ff
Ashwin Natarajan <ashwin.nat@gmail.com>
2025-10-22 11:42:35 +0530

    proper shutdown

--- apps/hud/ipc/task.py
+++ apps/hud/ipc/task.py
@@ -38,8 +38,10 @@ def run_ipc_task(port: int, logger: logging.Logger, window_manager: WindowManage
         port=port,
         name="hud"
     )
+    ipc_server.register_shutdown_callback(partial(
+        _shutdown_handler, logger=logger, window_manager=window_manager, stop_event=stop_event))
     return ipc_server.serve_in_thread(partial(
-        _ipc_handler, window_manager=window_manager, logger=logger, stop_event=stop_event))
+        _ipc_handler, logger=logger, window_manager=window_manager, stop_event=stop_event))
 
 def _ipc_handler(msg: dict, logger: logging.Logger, window_manager: WindowManager, stop_event: threading.Event) -> dict:
     """Handles incoming IPC messages and dispatches commands.
@@ -58,20 +60,31 @@ def _ipc_handler(msg: dict, logger: logging.Logger, window_manager: WindowManage
     if not (cmd := msg.get("cmd")):
         return {"status": "error", "message": "Missing command name"}
 
-    if cmd == "shutdown":
-        threading.Thread(target=_stop_other_tasks, args=(logger, window_manager, stop_event,)).start()
-        return {"status": "success",}
-    else:
-        return {"status": "error", "message": f"Unknown command: {cmd}"}
+    return {"status": "error", "message": f"Unknown command: {cmd}"}
 
-def _stop_other_tasks(logger: logging.Logger, window_manager: WindowManager, stop_event: threading.Event) -> None:
-    """Stop all other tasks when IPC shutdown is received.
+def _shutdown_handler(args: dict, logger: logging.Logger, window_manager: WindowManager, stop_event: threading.Event) -> None:
+    """Handles shutdown command.
 
     Args:
+        args (dict): IPC message
         logger (logging.Logger): Logger
         window_manager (WindowManager): WindowManager
         stop_event (threading.Event): Event to signal stopping
     """
-    logger.info("Shutdown command received via IPC. Stopping all tasks...")
+
+    threading.Thread(target=_stop_other_tasks, args=(args, logger, window_manager, stop_event,)).start()
+    return {"status": "success", "message": "Shutting down HUD manager"}
+
+def _stop_other_tasks(args: dict, logger: logging.Logger, window_manager: WindowManager, stop_event: threading.Event) -> None:
+    """Stop all other tasks when IPC shutdown is received.
+
+    Args:
+        args (dict): IPC message
+        logger (logging.Logger): Logger
+        window_manager (WindowManager): WindowManager
+        stop_event (threading.Event): Event to signal stopping
+    """
+    reason = args.get("reason", "N/A")
+    logger.info(f"Shutdown command received via IPC. Reason: {reason}. Stopping all tasks...")
     stop_event.set()
     window_manager.stop()
