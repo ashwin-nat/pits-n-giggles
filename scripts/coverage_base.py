d8f757d680cc5f9ad2ec98c6ff937a532ca870e3
Ashwin Natarajan <ashwin.nat@gmail.com>
2025-10-30 21:46:50 +0530

    save all coverage reports in coverage_reports/

--- scripts/coverage_base.py
+++ scripts/coverage_base.py
@@ -19,7 +19,7 @@ def run_coverage(RUN_TYPE, script, rcfile, script_args_str="", manage_coverage=T
 
     timestamp = start_time.strftime("%Y_%m_%d__%H_%M_%S")
 
-    htmlcov_dir = f"htmlcov_{timestamp}"
+    htmlcov_dir = f"coverage_reports/htmlcov_{timestamp}"
 
     if manage_coverage:
         os.environ["COVERAGE_PROCESS_START"] = rcfile
@@ -35,6 +35,9 @@ def run_coverage(RUN_TYPE, script, rcfile, script_args_str="", manage_coverage=T
     os.system("coverage combine")
     title = f"{RUN_TYPE.capitalize()} Test Coverage ({timestamp})"
     os.system(f'coverage html --title="{title}"')
+
+    # Ensure the coverage_reports directory exists
+    os.makedirs("coverage_reports", exist_ok=True)
 
     # Rename htmlcov folder to include timestamp for archival
     if os.path.isdir("htmlcov"):
