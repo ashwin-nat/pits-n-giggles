2929f2dcdc14ad736dd71fca9180fb89f4055d56
Ashwin Natarajan <ashwin.nat@gmail.com>
2025-11-11 01:47:24 +0530

    adding opacity to schema

--- tests/tests_config/tests_hud_settings.py
+++ tests/tests_config/tests_hud_settings.py
@@ -46,6 +46,7 @@ class TestHudSettings(TestF1ConfigBase):
         self.assertEqual(settings.toggle_overlays_udp_action_code, 10)
         self.assertEqual(settings.show_lap_timer, True)
         self.assertEqual(settings.show_timing_tower, True)
+        self.assertEqual(settings.overlays_opacity, 100)
 
     def test_enabled_validation(self):
         """Test valid and invalid log_file_size values"""
@@ -110,3 +111,22 @@ class TestHudSettings(TestF1ConfigBase):
 
         with self.assertRaises(ValidationError):
             HudSettings(show_timing_tower=420)
+
+    def test_overlays_opacity_validation(self):
+        """Test valid and invalid overlays_opacity values"""
+        # Valid value
+        opacity = 75
+        hud_settings = HudSettings(overlays_opacity=opacity)
+        self.assertEqual(hud_settings.overlays_opacity, opacity)
+
+        with self.assertRaises(ValidationError):
+            HudSettings(overlays_opacity=None)  # type: ignore
+
+        with self.assertRaises(ValidationError):
+            HudSettings(overlays_opacity="invalid")
+
+        with self.assertRaises(ValidationError):
+            HudSettings(overlays_opacity=-10)
+
+        with self.assertRaises(ValidationError):
+            HudSettings(overlays_opacity=150)
