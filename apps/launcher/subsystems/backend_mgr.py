3c1f711cf885424d27117e5493ea03100e26df09
Ashwin Natarajan <ashwin.nat@gmail.com>
2025-11-26 17:12:16 +0530

    backed restart on_settings_change all udp action codes will now trigger restart

--- apps/launcher/subsystems/backend_mgr.py
+++ apps/launcher/subsystems/backend_mgr.py
@@ -134,7 +134,13 @@ class BackendAppMgr(PngAppMgrBase):
             "Forwarding" : [],
             "StreamOverlay" : [],
             "HUD": [
+                "lap_timer_toggle_udp_action_code",
+                "timing_tower_toggle_udp_action_code",
+                "timing_tower_toggle_udp_action_code",
+                "mfd_toggle_udp_action_code",
                 "toggle_overlays_udp_action_code",
+                "cycle_mfd_udp_action_code",
+
             ],
             "TimeLossInPitsF1": [],
             "TimeLossInPitsF2": [],
