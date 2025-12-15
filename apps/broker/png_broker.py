3ca16885b854d050523324775848415f4948d2f8
Ashwin Natarajan <ashwin.nat@gmail.com>
2025-12-15 22:05:42 +0530

    actual broker functionality

--- apps/broker/png_broker.py
+++ apps/broker/png_broker.py
@@ -24,15 +24,18 @@
 
 import argparse
 import logging
+import os
 import sys
+from functools import partial
 
-from lib.child_proc_mgmt import report_pid_from_child
+from lib.child_proc_mgmt import (notify_parent_init_complete,
+                                 report_ipc_port_from_child,
+                                 report_pid_from_child)
 from lib.config import PngSettings, load_config_from_json
+from lib.error_status import PNG_LOST_CONN_TO_PARENT
+from lib.ipc import IpcPubSubBroker, IpcServerSync
 from lib.logger import get_logger
 from meta.meta import APP_NAME
-
-from lib.ipc import IpcPubSubBroker, IpcServerSync
-from .ipc import init_ipc_task
 
 # -------------------------------------- FUNCTIONS ---------------------------------------------------------------------
 
@@ -65,7 +68,27 @@ def main(logger: logging.Logger, config: PngSettings, debug_mode: bool) -> None:
         debug_mode (bool): Debug mode
     """
 
-    init_ipc_task(logger)
+    broker = IpcPubSubBroker(
+        xsub_port=config.Network.broker_xsub_port,
+        xpub_port=config.Network.broker_xpub_port,
+        logger=logger,
+        name="PitWall")
+    broker_thread = broker.run_in_thread()
+
+    proc_mgr = IpcServerSync(
+        name="pitwall_ipc",
+        max_missed_heartbeats=3,
+        heartbeat_timeout=5.0,
+    )
+    report_ipc_port_from_child(proc_mgr.port)
+    notify_parent_init_complete()
+
+    proc_mgr.register_shutdown_callback(partial(_proc_mgr_shutdown_callback, logger=logger, broker=broker))
+    proc_mgr.register_heartbeat_missed_callback(partial(_handle_heartbeat_missed, logger=logger))
+    proc_mgr.serve(handler_fn=_proc_mgmt_cmd_handler, timeout=0.25)
+
+    broker_thread.join(timeout=3.0)
+    proc_mgr.close()
 
 # -------------------------------------- ENTRY POINT -------------------------------------------------------------------
 
@@ -88,3 +111,23 @@ def entry_point():
         sys.exit(1)
 
     png_logger.info("PitWall application exiting normally.")
+
+# -------------------------------------- CALLBACKS ---------------------------------------------------------------------
+
+def _proc_mgr_shutdown_callback(_args: dict, logger: logging.Logger, broker: IpcPubSubBroker) -> dict:
+    """Shutdown callback"""
+    logger.debug("Received IPC shutdown. Shutting down the broker...")
+    broker.close()
+    return {"status": "success"}
+
+def _handle_heartbeat_missed(count: int, logger: logging.Logger) -> dict:
+    """Handle terminate command"""
+    logger.warning(f"Missed heartbeat {count} times. This process has probably been orphaned. Terminating...")
+    os._exit(PNG_LOST_CONN_TO_PARENT)
+
+def _proc_mgmt_cmd_handler(request: dict) -> dict:
+    """Handles incoming IPC messages and dispatches commands."""
+    return {
+        "status": "error",
+        "message": f"Pit Wall does not support IPC requests. Unknown command {request['cmd']}",
+    }
