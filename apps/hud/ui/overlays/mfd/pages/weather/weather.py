bed6375505b0ac6d26e1f8fde4b94ad11a512725
Ashwin Natarajan <ashwin.nat@gmail.com>
2026-02-07 19:12:39 +0530

    weather mfd interact -> handle div by 0

--- apps/hud/ui/overlays/mfd/pages/weather/weather.py
+++ apps/hud/ui/overlays/mfd/pages/weather/weather.py
@@ -80,6 +80,9 @@ class WeatherForecastPage(MfdPageBase):
         @self.on_event("mfd_interact")
         def mfd_interact(data: Dict[str, Any]) -> None:
             self.logger.debug("%s | Received mfd_interact command. args: %s", self.KEY, data)
+            if not self.num_sessions:
+                self.logger.debug("%s | No sessions to cycle through.", self.KEY)
+                return
             # Cycle through the sessions
             self.session_index = (self.session_index + 1) % self.num_sessions
             last_data = self._last_processed_samples
