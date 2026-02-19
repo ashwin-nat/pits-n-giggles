a5b24254a6ff2f5aec5a615be46a928d81d5f59d
Ashwin Natarajan <ashwin.nat@gmail.com>
2026-02-19 14:45:06 +0530

    remove formatting from stats

--- apps/launcher/gui/main_window.py
+++ apps/launcher/gui/main_window.py
@@ -700,7 +700,7 @@ class PngLauncherWindow(QMainWindow):
 
         stats = {subsystem.DISPLAY_NAME: subsystem.get_stats() for subsystem in self.subsystems}
         self.info_log(f"{APP_NAME} {self.ver_str} shutdown complete (forced={forced_shutdown}).")
-        self.logger.info("Final subsystem stats: %s", json.dumps(stats, indent=2))
+        self.logger.info("Final subsystem stats: %s", json.dumps(stats, indent=0, sort_keys=True))
         event.accept()
 
     def run(self):
