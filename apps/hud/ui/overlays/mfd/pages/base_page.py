926225a705528dcea63b09e728ea4414b89d01e8
Ashwin Natarajan <ashwin.nat@gmail.com>
2026-03-22 14:25:23 +0530

    stats for dropped events due to page not avlb

--- apps/hud/ui/overlays/mfd/pages/base_page.py
+++ apps/hud/ui/overlays/mfd/pages/base_page.py
@@ -64,8 +64,9 @@ class MfdPageBase:
         """
         def decorator(fn):
             if requires_page_item:
-                def wrapper(data):
+                def wrapper(data, _event_type=event_type):
                     if not self._page_item:
+                        self._stats.track_event("__DROPPED_NO_PAGE__", _event_type)
                         return
                     return fn(data)
                 self._handlers[event_type] = wrapper
